67a9c79b719008b77424c5efdc89f55871e5d217
bmonagan <bernardmonagan@gmail.com>
2025-12-22 14:05:13 -0500

    Tested the classes and functions in main.py to ensure they work as expected.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
-print("hello world!")
+from textnode import TextNode,TextType
+def main():
+    test_node = TextNode("dance",TextType.PLAIN,"spacestation.com")
+    print(test_node)
+
+    
+
+
+if __name__ == "__main__":
+    main()
 
