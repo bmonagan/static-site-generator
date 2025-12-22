0a0c6a84c4339f2a4d69696bdb033e15db822d69
bmonagan <bernardmonagan@gmail.com>
2025-12-22 14:15:03 -0500

    Addded in more tests of the text type and node classes.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,23 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    def test_eq2(self):
+        node = TextNode("This is a text node", TextType.BOLD,"space_station")
+        node2 = TextNode("This is a text node", TextType.BOLD,"space_station")
+        self.assertEqual(node, node2)
+    def test_differentURL(self):
+        node = TextNode("This is a text node", TextType.BOLD,"space_station")
+        node2 = TextNode("This is a text node", TextType.BOLD,"NOTspace_station")
+        self.assertNotEqual(node, node2)
+    def test_URLeqNONE(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD,None )
+        self.assertEqual(node, node2)
+    def test_textType_notEqual(self):
+        node = TextNode("This is a text node", TextType.PLAIN,"space_station")
+        node2 = TextNode("This is a text node", TextType.BOLD,"space_station")
+        self.assertNotEqual(node, node2)
+
 
 
 if __name__ == "__main__":
