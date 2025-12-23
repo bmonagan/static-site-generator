2b69e99bb13566e8d948af656166d5b70c6e44ac
bmonagan <bernardmonagan@gmail.com>
2025-12-23 18:43:55 -0500

    Updated testing for split nodes. Passing all at this point.

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -1,50 +1,97 @@
 import unittest
 from textnode import TextType, TextNode
+from splitnodes import split_nodes_delimiter
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     
     def test_single_match(self):
-        input_node = TextNode("This is a `code block`.", TextType.TEXT)
+        old_nodes = [TextNode("This is **bold** text.", TextType.TEXT)]
+        delimiter = "**"
+        
         expected_nodes = [
-            TextNode("This is a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(".", TextType.TEXT),
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text.", TextType.TEXT),
         ]
-        self.assertEqual(len(expected_nodes), 3)
+        
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.BOLD)
+        self.assertEqual(result_nodes, expected_nodes)
 
     def test_multiple_matches(self):
-        input_node = TextNode("The **quick** brown fox jumped **over** the lazy dog.", TextType.TEXT)
+        old_nodes = [TextNode("`First` and `Second` blocks.", TextType.TEXT)]
+        delimiter = "`"
+        
         expected_nodes = [
-            TextNode("The ", TextType.TEXT),
-            TextNode("quick", TextType.BOLD),
-            TextNode(" brown fox jumped ", TextType.TEXT),
-            TextNode("over", TextType.BOLD),
-            TextNode(" the lazy dog.", TextType.TEXT),
+            TextNode("First", TextType.CODE),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("Second", TextType.CODE),
+            TextNode(" blocks.", TextType.TEXT),
         ]
-        self.assertEqual(len(expected_nodes), 5)
+        
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.CODE)
+        self.assertEqual(result_nodes, expected_nodes)
         
     def test_delimiter_at_edges(self):
-        input_node = TextNode("**Bold text** in the middle and **end**.", TextType.TEXT)
+        old_nodes = [TextNode("~~italic at start and end~~", TextType.TEXT)]
+        delimiter = "~~"
+        
         expected_nodes = [
-            TextNode("Bold text", TextType.BOLD),
-            TextNode(" in the middle and ", TextType.TEXT),
-            TextNode("end", TextType.BOLD),
-            TextNode(".", TextType.TEXT),
+            TextNode("italic at start and end", TextType.ITALIC),
         ]
-        self.assertEqual(len(expected_nodes), 4)
+        
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.ITALIC)
+        self.assertEqual(result_nodes, expected_nodes)
 
     def test_no_delimiter_present(self):
-        input_node = TextNode("This text has no special formatting.", TextType.TEXT)
+        old_nodes = [TextNode("This text has no special formatting.", TextType.TEXT)]
+        delimiter = "*"
+        
         expected_nodes = [
             TextNode("This text has no special formatting.", TextType.TEXT),
         ]
-        self.assertEqual(len(expected_nodes), 1)
-
-    def test_mismatched_delimiter_raises_error(self):
-        input_node = TextNode("This is missing a closing `delimiter.", TextType.TEXT)
         
-        with self.assertRaises(ValueError):
-            pass
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.ITALIC)
+        self.assertEqual(result_nodes, expected_nodes)
+        
+    def test_mismatched_delimiter_raises_error(self):
+        old_nodes = [TextNode("This is missing a closing `delimiter.", TextType.TEXT)]
+        delimiter = "`"
+        
+        with self.assertRaisesRegex(ValueError, "Mismatched delimiter"):
+            split_nodes_delimiter(old_nodes, delimiter, TextType.CODE)
+            
+    def test_handling_non_text_node(self):
+        old_nodes = [
+            TextNode("Text before.", TextType.TEXT),
+            TextNode("A link", TextType.LINK, "http://example.com"),
+            TextNode(" and `code` here.", TextType.TEXT)
+        ]
+        delimiter = "`"
+        
+        expected_nodes = [
+            TextNode("Text before.", TextType.TEXT),
+            TextNode("A link", TextType.LINK, "http://example.com"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode(" here.", TextType.TEXT),
+        ]
+        
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.CODE)
+        self.assertEqual(result_nodes, expected_nodes)
+        
+    def test_empty_string_between_delimiters(self):
+        old_nodes = [TextNode("Example **empty** **test**", TextType.TEXT)]
+        delimiter = "**"
+        
+        expected_nodes = [
+            TextNode("Example ", TextType.TEXT),
+            TextNode("empty", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("test", TextType.BOLD),
+        ]
+        
+        result_nodes = split_nodes_delimiter(old_nodes, delimiter, TextType.BOLD)
+        self.assertEqual(result_nodes, expected_nodes)
 
 if __name__ == '__main__':
     unittest.main()
