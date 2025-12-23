0a1e9e76bba8fa40ea88c1d7d087785cd439bf25
bmonagan <bernardmonagan@gmail.com>
2025-12-23 18:43:10 -0500

    Modified split function to handle non-matched delimiters and to properly always append the new node to the list.eeeeeeee

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,5 +1,4 @@
 from textnode import TextNode,TextType
-f
 def split_nodes_delimiter(old_nodes,delimiter,text_type):
     new_nodes = []
 
@@ -8,17 +7,19 @@ def split_nodes_delimiter(old_nodes,delimiter,text_type):
             new_nodes.append(node)
             continue
 
-        section = node.split(delimiter)
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError(f"Mismatched delimiter: {delimiter}")
         split_nodes = []
-        for i in range(len(section)):
-            part = section[i]
+        for i in range(len(sections)):
+            part = sections[i]
             if part == "":
                 continue
             if i % 2 == 0:
                 node = TextNode(part, TextType.TEXT)
             else:
                 node = TextNode(part, text_type)
-                split_nodes.append(node)
+            split_nodes.append(node)
         new_nodes.extend(split_nodes)
     
     return new_nodes
