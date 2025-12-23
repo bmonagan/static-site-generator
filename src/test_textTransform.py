ebecb650b24faed55c5fa02876a6b57376849642
bmonagan <bernardmonagan@gmail.com>
2025-12-22 23:59:59 -0500

    Added more tests that are currently working.

--- src/test_textTransform.py
+++ src/test_textTransform.py
@@ -1,6 +1,6 @@
 import unittest
 from textTransform import text_node_to_html_node
-from textnode import TextNode,TextType
+from textnode import TextNode, TextType
 
 class TestTexToHTML(unittest.TestCase):
     def test_text(self):
@@ -8,7 +8,30 @@ class TestTexToHTML(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
-    
+
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+        self.assertEqual(html_node.props, None)
+
+    def test_link(self):
+        url = "https://example.com"
+        node = TextNode("This is a link", TextType.LINK, url)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props, {"href": url})
+
+    def test_image(self):
+        url = "https://image.com/photo.jpg"
+        alt_text = "A picture of a dog"
+        node = TextNode(alt_text, TextType.IMAGE, url)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(html_node.props, {"src": url, "alt": alt_text})
 
 if __name__ == "__main__":
     unittest.main()
