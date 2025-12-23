6b987640bcafa2757c2175dd5e99418ac58dc032
bmonagan <bernardmonagan@gmail.com>
2025-12-22 20:15:48 -0500

    fixed the variables to be in the correct order.

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
 
-    def __init__(self,value,tag, props=None):
+    def __init__(self,tag =None,value=None, props=None):
         super().__init__(value,tag,props)
     
     def to_html(self):
