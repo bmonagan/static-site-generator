3fb452be1483a8e1811d66a0d4cbee81f51ce822
bmonagan <bernardmonagan@gmail.com>
2025-12-22 20:13:19 -0500

    Created logic for leafnode.py to handle html with no children.

---
+++ src/leafnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+
+    def __init__(self,value,tag, props=None):
+        super().__init__(value,tag,props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return str(self.value)
+        else:
+            if not self.props:
+                return f"<{self.tag}>{self.value}</{self.tag}>"
+            else:
+                html_props = self.props_to_html()
+                return f"<{self.tag} {html_props}>{self.value}</{self.tag}>"
+
