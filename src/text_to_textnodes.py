3776fb0211234d91e7fa447c108b75316557a6f4
bmonagan <bernardmonagan@gmail.com>
2025-12-24 02:41:46 -0500

    removed inital testing from text to textnodes file

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -20,25 +20,3 @@ def text_to_textnodes(text):
    
     
     return result
-
-
-
-test = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-res = text_to_textnodes(test)
-print(res)
-
-
-desired_output = [
-    TextNode("This is ", TextType.TEXT),
-    TextNode("text", TextType.BOLD),
-    TextNode(" with an ", TextType.TEXT),
-    TextNode("italic", TextType.ITALIC),
-    TextNode(" word and a ", TextType.TEXT),
-    TextNode("code block", TextType.CODE),
-    TextNode(" and an ", TextType.TEXT),
-    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-    TextNode(" and a ", TextType.TEXT),
-    TextNode("link", TextType.LINK, "https://boot.dev"),
-]
-
-assert res == desired_output
