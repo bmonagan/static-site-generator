376f0f098386fc2307d2e9f69802fc9d4f0cc99c
bmonagan <bernardmonagan@gmail.com>
2025-12-24 02:43:05 -0500

    Initial test moved and still working, going to add a few more tests and try them out.

---
+++ src/test_text_to_leafnodes.py
@@ -0,0 +1,24 @@
+import unittest
+from text_to_textnodes import text_to_textnodes
+from textnode import TextNode, TextType
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_basic_output(self):
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+
+if __name__ == '__main__':
+    unittest.main()
