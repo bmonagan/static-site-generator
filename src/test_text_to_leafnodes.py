0834933ce875192856d0bde7b6ce7ea29f84d5bb
bmonagan <bernardmonagan@gmail.com>
2025-12-24 02:44:32 -0500

    Currently failing  ValueError: Mismatched delimiter: _on test_adjacent_formatting_and_links.

--- src/test_text_to_leafnodes.py
+++ src/test_text_to_leafnodes.py
@@ -1,6 +1,7 @@
 import unittest
 from text_to_textnodes import text_to_textnodes
 from textnode import TextNode, TextType
+
 class TestMarkdownExtraction(unittest.TestCase):
     def test_basic_output(self):
         expected_nodes = [
@@ -16,6 +17,65 @@ class TestMarkdownExtraction(unittest.TestCase):
             TextNode("link", TextType.LINK, "https://boot.dev"),
         ]
         text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+    def test_only_unformatted_text(self):
+        text = "Just plain text here."
+        expected_nodes = [
+            TextNode(text, TextType.TEXT)
+        ]
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+    def test_adjacent_formatting_and_links(self):
+        text = "Here is a**bold****text**_italic_[link](url)![](img_url)"
+        expected_nodes = [
+            TextNode("Here is a", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode("text", TextType.BOLD),
+            TextNode("italic", TextType.ITALIC),
+            TextNode("link", TextType.LINK, "url"),
+            TextNode("", TextType.IMAGE, "img_url"),
+        ]
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+    def test_start_and_end_with_formatting(self):
+        text = "**Start bold** and some text, then `end code`."
+        expected_nodes = [
+            TextNode("Start bold", TextType.BOLD),
+            TextNode(" and some text, then ", TextType.TEXT),
+            TextNode("end code", TextType.CODE),
+            TextNode(".", TextType.TEXT)
+        ]
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+    def test_multiple_images_and_links(self):
+        text = "[Link1](L1) ![ImageA](IA) text ![ImageB](IB) [Link2](L2)."
+        expected_nodes = [
+            TextNode("Link1", TextType.LINK, "L1"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("ImageA", TextType.IMAGE, "IA"),
+            TextNode(" text ", TextType.TEXT),
+            TextNode("ImageB", TextType.IMAGE, "IB"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("Link2", TextType.LINK, "L2"),
+            TextNode(".", TextType.TEXT)
+        ]
+        result_nodes = text_to_textnodes(text)
+        self.assertEqual(result_nodes, expected_nodes)
+
+    def test_only_one_type_of_formatting(self):
+        text = "This is _just_ an italic text _example_."
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("just", TextType.ITALIC),
+            TextNode(" an italic text ", TextType.TEXT),
+            TextNode("example", TextType.ITALIC),
+            TextNode(".", TextType.TEXT),
+        ]
         result_nodes = text_to_textnodes(text)
         self.assertEqual(result_nodes, expected_nodes)
 
