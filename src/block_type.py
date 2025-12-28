0694bebf6777e73d5846ff56d13aa524c76edc93
bmonagan <bernardmonagan@gmail.com>
2025-12-27 22:12:52 -0500

    Added better handling in the ordered list area of the function to account for edge cases.

--- src/block_type.py
+++ src/block_type.py
@@ -22,15 +22,18 @@ def block_to_block_type(markdown):
     elif all(line[0] == "-" for line in line_split ):
         return BlockType.UNORDERED_LIST
     elif all(line[0].isnumeric() for line in line_split):
-        if line_split[0][1] != ".":
-            return BlockType.PARAGRAPH
-        current_num = int(line_split[0][0])
+        dot_index = line_split[0].find(".")
+        if dot_index == -1:
+                return BlockType.PARAGRAPH
+        current_num = int(line_split[0][0:dot_index])
         for line in line_split[1:]:
-            line_number = int(line[0])
-            if line_number != current_num + 1 or line[1] != ".":
+            dot_index = line.find(".")
+            if dot_index == -1:
+                return BlockType.PARAGRAPH
+            line_number = int(line[0:dot_index])
+            if line_number != current_num + 1:
                 return BlockType.PARAGRAPH
             current_num = line_number
-            
         return BlockType.ORDERED_LIST
     
     return BlockType.PARAGRAPH
