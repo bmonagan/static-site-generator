06db1e6e0711ed114beead96ae9fe91d8306b3ae
bmonagan <bernardmonagan@gmail.com>
2025-12-27 22:02:12 -0500

    Corrected the all() sections of the check to correctly check and added in some testing that is working right now.

--- src/block_type.py
+++ src/block_type.py
@@ -17,9 +17,9 @@ def block_to_block_type(markdown):
     elif md_len > 6 and markdown[:3] == markdown[md_len-3 : md_len] == "'''":
         return BlockType.CODE
     line_split = markdown.split("\n")
-    if all(line[0] for line in line_split == ">"):
+    if all(line[0]  == ">" for line in line_split):
         return BlockType.QUOTE
-    elif all(line[0] for line in line_split == "-"):
+    elif all(line[0] == "-" for line in line_split ):
         return BlockType.UNORDERED_LIST
     elif all(line[0].isnumeric() for line in line_split):
         if line_split[0][1] != ".":
@@ -33,6 +33,13 @@ def block_to_block_type(markdown):
         return BlockType.ORDERED_LIST
     
     return BlockType.PARAGRAPH
+
+blocks = ["This is **bolded** paragraph","This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line","- This is a list\n- with items"]
+
+for block in blocks:
+    res = block_to_block_type(block)
+    print(res)
+        
     
      
             
