85f1557b7f5e283da4966d6157c795cf85b71fc3
bmonagan <bernardmonagan@gmail.com>
2025-12-27 21:37:32 -0500

    Added more tests which are working at this point.

--- src/test_split_blocks.py
+++ src/test_split_blocks.py
@@ -21,9 +21,46 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_empty_string(self):
+        md = ""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, [])
+
+    def test_only_whitespace(self):
+        md = " \n\n \t\n\n "
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks, [])
+
+    def test_multiple_empty_lines_between_blocks(self):
+        md = """
+# Header 1
+
+
+This is paragraph one.
 
 
 
+Paragraph two with many blank lines.
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "# Header 1",
+                "This is paragraph one.",
+                "Paragraph two with many blank lines.",
+            ],
+        )
+    def test_block_with_leading_and_trailing_spaces(self):
+        md = "  A block with spaces \n on newlines \n\n Another block "
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "A block with spaces \n on newlines",
+                "Another block",
+            ],
+        )
 
 
 if __name__ == '__main__':
