b153afb03469a5d3f6761cb7fc0eb4c892622a83
bmonagan <bernardmonagan@gmail.com>
2025-12-29 19:21:46 -0500

    Removed older testing from block type file.

--- src/block_type.py
+++ src/block_type.py
@@ -38,12 +38,6 @@ def block_to_block_type(markdown):
     
     return BlockType.PARAGRAPH
 
-blocks = ["This is **bolded** paragraph","This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line","- This is a list\n- with items"]
-
-for block in blocks:
-    res = block_to_block_type(block)
-    print(res)
-        
     
      
             
