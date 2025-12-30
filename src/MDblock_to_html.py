eeb8ddfedca1df9d1e90cbba73062d537ae91953
bmonagan <bernardmonagan@gmail.com>
2025-12-29 20:02:23 -0500

    Not confident about this going into testing. Probably needs a decent refactor because of how the new lines are outputting. There is also another print statement nested somewhere in the dependencies that i need to find at some point.

--- src/MDblock_to_html.py
+++ src/MDblock_to_html.py
@@ -3,19 +3,26 @@ from markdown_blocks import markdown_to_blocks
 from text_to_textnodes import text_to_textnodes
 from parentnode import ParentNode
 from text_to_children import text_to_children
+from textnode import TextNode, TextType
+from textTransform import text_node_to_html_node
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     if not blocks:
         raise ValueError("Must be a non-empty markdown")
-    print(blocks)
+    complete_children = []
     for block in blocks:
         parent_tag = block_to_block_type(block)
         if parent_tag == BlockType.CODE:
-            # textnode -> text node to html node
+            code_node = TextNode(block, TextType.CODE)
+            code_htmlnode = text_node_to_html_node(code_node)
+            complete_children.append(code_htmlnode) 
         else:
             children_nodes = text_to_children(block)
-        
+            smaller_parent = ParentNode(tag = parent_tag, children = children_nodes)
+            complete_children.append(smaller_parent)
+    parent_html_node = ParentNode(tag= "div", children= complete_children)
+    return parent_html_node
 
 
 
@@ -28,4 +35,4 @@ This is another paragraph with _italic_ text and `code` here
 
 """
 
-markdown_to_html_node(md)
+print(markdown_to_html_node(md))
