9d681ecb128bd4fcfab3344294fea9754fdf6253
bmonagan <bernardmonagan@gmail.com>
2026-01-12 22:22:33 -0500

    Removed intermediate testing from mdblock to html.

--- src/MDblock_to_html.py
+++ src/MDblock_to_html.py
@@ -96,18 +96,3 @@ def markdown_to_html_node(markdown):
     
     parent_html_node = ParentNode(tag="div", children=complete_children)
     return parent_html_node
-
-
-md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-This is another paragraph with _italic_ text and `code` here
-
-"""
-desired_output =  "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
-
-output = markdown_to_html_node(md).to_html()
-print(output)
-assert output == desired_output
