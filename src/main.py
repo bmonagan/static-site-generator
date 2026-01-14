6840756e3993af6026d680f9004b6d10fc65b87f
bmonagan <bernardmonagan@gmail.com>
2026-01-13 21:40:03 -0500

    Updated main.py to include the generate page function.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import TextNode,TextType
 from file_management import copy_static_to_public
+from page_generation import generate_page
 def main():
     copy_static_to_public()
     print("Static files copied to public directory successfully.")
+
+    generate_page(from_path="./content/index.md", template_path="./template.html", dest_path="./public/index.html")
 
     
 
