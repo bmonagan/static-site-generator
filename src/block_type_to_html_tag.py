cda78ac395b36b4f14cb90cff68f4bb6d2f58083
bmonagan <bernardmonagan@gmail.com>
2026-01-15 15:15:51 -0500

    Added handling for headers other than h1.

--- src/block_type_to_html_tag.py
+++ src/block_type_to_html_tag.py
@@ -9,8 +9,22 @@ html_tag_map = {
     BlockType.ORDERED_LIST: "ol"
 }
 
+heading_tag_map = {
+    1: "h1",
+    2: "h2",
+    3: "h3",
+    4: "h4",
+    5: "h5",
+    6: "h6"
+}
 
-def block_type_to_html_tag(block_type):
+
+def block_type_to_html_tag(block_type, heading_level=None):
+    if block_type == BlockType.HEADING:
+        if heading_level is not None:
+            return heading_tag_map.get(heading_level, "h1")
+        return "h1"
+    
     if block_type not in html_tag_map:
         raise ValueError("Must be a block type")
     else:
