94a472a88ac38348724dd876cadca56368885eb6
bmonagan <bernardmonagan@gmail.com>
2026-01-15 15:31:47 -0500

    Changed main to use the recursive page generation function.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from textnode import TextNode,TextType
 from file_management import copy_static_to_public
-from page_generation import generate_page
+from page_generation import generate_pages_recursive
 def main():
     copy_static_to_public()
     print("Static files copied to public directory successfully.")
 
-    generate_page(from_path="./content/index.md", template_path="./template.html", dest_path="./public/index.html")
+    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public")
+    print("All pages generated successfully.")
 
     
 
