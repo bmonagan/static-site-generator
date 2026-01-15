7040e25b046fd269727d4679d683f76df7e33fe8
bmonagan <bernardmonagan@gmail.com>
2026-01-14 21:19:17 -0500

    restructured leaf node based on testing further upstream.

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,22 +1,17 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-
-    def __init__(self,tag =None,value=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag=tag, value=value, children=None, props=props)
-    
-    def to_html(self):
-        if not self.value:
-            raise ValueError("All leaf nodes must have a value")
-        
-        if not self.tag:
-            return str(self.value)
-        
-        else:
-            html_props = self.props_to_html() 
-           
-            if html_props:
-                return f"<{self.tag} {html_props}>{self.value}</{self.tag}>"
-            else:
-                return f"<{self.tag}>{self.value}</{self.tag}>"
 
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+
+        if self.tag is None:
+            return str(self.value)
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
