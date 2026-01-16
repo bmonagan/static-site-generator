4a4f18fe9c95777add5adada2b2181f1e97f5ac1
bmonagan <bernardmonagan@gmail.com>
2026-01-15 19:43:08 -0500

    changing from public folder to docs to align with github pages.

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from file_management import copy_static_to_public
 from page_generation import generate_pages_recursive
 def main():
     copy_static_to_public()
-    print("Static files copied to public directory successfully.")
+    print("Static files copied to docs directory successfully.")
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public",basepath=basepath)
+    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="docs",basepath=basepath)
     print("All pages generated successfully.")
 
     
