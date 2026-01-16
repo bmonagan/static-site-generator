5b186232e84d9ad4296178bb854ae61b6c2d57a3
bmonagan <bernardmonagan@gmail.com>
2026-01-15 19:41:29 -0500

    Fixed the handling of basepath inside of the page generation functions.

--- src/page_generation.py
+++ src/page_generation.py
@@ -2,7 +2,7 @@ import os
 from extract_title import extract_title
 from MDblock_to_html import markdown_to_html_node
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path,basepath="/"):
     print(f"Generating page from {from_path} to {dest_path}{template_path}")
     markdown = open(from_path, "r").read()
     template = open(template_path, "r").read()
@@ -18,12 +18,12 @@ def generate_page(from_path, template_path, dest_path):
         f.write(final_html)
     print(f"Page generated successfully at {dest_path}")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path,basepath="/"):
     for entry in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, entry)
         if os.path.isdir(entry_path):
             # Recursively process subdirectories
-            generate_pages_recursive(entry_path, template_path, os.path.join(dest_dir_path, entry))
+            generate_pages_recursive(entry_path, template_path, os.path.join(dest_dir_path, entry),basepath = basepath)
         elif entry.endswith(".md"):
             # Generate page for markdown files
             dest_path = os.path.join(dest_dir_path, entry[:-3] + ".html")
