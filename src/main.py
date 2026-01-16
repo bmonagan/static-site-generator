1c0b40c450fccd6cf6aea80dd07099dfab5e2c76
bmonagan <bernardmonagan@gmail.com>
2026-01-15 19:24:29 -0500

    Changing main.py to accept a cli argument for the content directory.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
+import sys
 from textnode import TextNode,TextType
 from file_management import copy_static_to_public
 from page_generation import generate_pages_recursive
 def main():
     copy_static_to_public()
     print("Static files copied to public directory successfully.")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "content"
 
-    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public")
+    generate_pages_recursive(dir_path_content= basepath, template_path="template.html", dest_dir_path="public")
     print("All pages generated successfully.")
 
     
